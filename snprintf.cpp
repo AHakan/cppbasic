8f2f7d4709bf4f64be18ae43ac03f131e0f7191b
Ahmet Hakan Çelik <ahmethcelik@gmail.com>
2021-12-15 13:25:35 +0300

    fixed

--- snprintf.cpp
+++ snprintf.cpp
@@ -1,6 +1,11 @@
-// sprintf() fonksiyonu, string (dizgi) içerisine yazdırmak için kullanılır ve ilk karakteri de buradan gelmektedir. Bu fonksiyonun en önemli özelliği, bastırdığı değerlerin sonuna dizgi sonu (end of string) anlamnda ‘’ karakteri koymasıdır.
+// sprintf() fonksiyonu, string (dizgi) içerisine yazdırmak için kullanılır ve
+// ilk karakteri de buradan gelmektedir. Bu fonksiyonun en önemli özelliği,
+// bastırdığı değerlerin sonuna dizgi sonu (end of string) anlamnda ‘’ karakteri
+// koymasıdır.
 
-// snprintf() fonksiyonu ise, sprintf() fonksiyonu ile tamamen aynı çalışmakta olup, tek farkı ilave bir sayı alıyor olmasıdır. Bu sayede kaç karakterin basılacağı fonksiyon çağırılırken belirtilebilir.
+// snprintf() fonksiyonu ise, sprintf() fonksiyonu ile tamamen aynı çalışmakta
+// olup, tek farkı ilave bir sayı alıyor olmasıdır. Bu sayede kaç karakterin
+// basılacağı fonksiyon çağırılırken belirtilebilir.
 
 #include <cstdio>
 #include <iostream>
@@ -9,22 +14,19 @@ using namespace std;
 
 #define MAX_BUFFER_SIZE 32768
 
-int main()
-{
-    char buffer[MAX_BUFFER_SIZE];
-    int retVal;
-    char name[] = "Max";
-    int age = 23;
+int main() {
+  char buffer[MAX_BUFFER_SIZE];
+  int retVal;
+  char name[] = "Max";
+  int age = 23;
 
-    retVal = snprintf(buffer, MAX_BUFFER_SIZE, "Hi, I am %s and I am %d years old.", name, age);
-    if (retVal > 0 && retVal < MAX_BUFFER_SIZE)
-    {
-        cout << buffer << endl;
-        cout << "Number of characters written = " << retVal << endl;
-    }
-    else
-        cout << "Error writing to buffer" << endl;
+  retVal = snprintf(buffer, MAX_BUFFER_SIZE,
+                    "Hi, I am %s and I am %d years old.", name, age);
+  if (retVal > 0 && retVal < MAX_BUFFER_SIZE) {
+    cout << buffer << endl;
+    cout << "Number of characters written = " << retVal << endl;
+  } else
+    cout << "Error writing to buffer" << endl;
 
-    return 0;
+  return 0;
 }
-
